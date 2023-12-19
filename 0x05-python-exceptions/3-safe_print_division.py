c8b3d1e49adef5c5d70ddf9bcb937598df4cae55
issamhilli <issamhilli9@gmail.com>
2023-12-19 04:21:01 -0800

    exceptions

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,12 @@
+3-safe_print_division.py
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    """Returns the division of a by b."""
+    try:
+        div = a / b
+    except (TypeError, ZeroDivisionError):
+        div = None
+    finally:
+        print("Inside result: {}".format(div))
+    return (div)
---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,6 @@
+5-raise_exception.py
+#!/usr/bin/python3
+
+def raise_exception():
+    """Raise a TypeError exception."""
+    raise TypeError
